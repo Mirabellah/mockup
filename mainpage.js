c9cf7b39f2622ab19135a3d26e582fed49cde2f5
Mirabellah <224236@buas.nl>
2024-03-19 18:27:36 +0100

    change

--- mainpage.js
+++ mainpage.js
@@ -15,26 +15,4 @@ document.addEventListener("DOMContentLoaded", function() {
         // For example, you can send the form data to a server for processing, or update the UI with search results.
     });
 });
-const testimonials = document.querySelectorAll('.testimonial');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
 
-let index = 0;
-
-function showTestimonial(index) {
-  testimonials.forEach((testimonial, i) => {
-    testimonial.style.display = i === index ? 'block' : 'none';
-  });
-}
-
-prevBtn.addEventListener('click', () => {
-  index = (index - 1 + testimonials.length) % testimonials.length;
-  showTestimonial(index);
-});
-
-nextBtn.addEventListener('click', () => {
-  index = (index + 1) % testimonials.length;
-  showTestimonial(index);
-});
-
-showTestimonial(index); // Show initial testimonial
